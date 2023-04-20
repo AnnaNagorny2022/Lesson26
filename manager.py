1c07eee941a50360c60f38ec529a8cdfc8fa6ea6
AnnaNagorny2022 <a.melyukh@mail.ru>
2023-04-20 20:13:55 +0300

    bus,mesta

---
+++ manager.py
@@ -0,0 +1,18 @@
+from bus import Bus
+
+
+# class Manager:
+#     pass
+
+def find_bus(buses):
+    if not isinstance(buses, (list, tuple)):
+        return Bus()
+
+    target = buses[0]
+
+    for bus in buses:
+        if isinstance(bus, Bus):
+            if (bus.price / bus.number) < (target.price / target.number):
+                target = bus
+
+    return target
